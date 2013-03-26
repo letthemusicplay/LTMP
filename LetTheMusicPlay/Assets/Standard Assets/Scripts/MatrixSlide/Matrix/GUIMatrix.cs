92f2f370ea298f2762c2edfdc588b64ec00ea664
William <william.besnard@hotmail.fr>
2013-03-26 17:16:27 +0100

    tentative de resolution de probleme

--- LetTheMusicPlay/Assets/Standard Assets/Scripts/MatrixSlide/Matrix/GUIMatrix.cs
+++ LetTheMusicPlay/Assets/Standard Assets/Scripts/MatrixSlide/Matrix/GUIMatrix.cs
@@ -8,23 +8,24 @@ using UnityEngine;
 		/// The size of rects in percent of screen dimensions.
 		/// </summary>
 		public float rectPercent;
-		/// <summary>
-		/// The position of the center of the matric, in screen percent
-		/// </summary>
-		public Vector2 posPercent;
 		private Rect[,] rects;
 	
 		public void Create(){
 			rects=new Rect[size,size];
+			
 			for(int i=0;i<size;i++){
 				for(int j=0;j<size;j++){
-					rects[i,j]=new Rect();
-				
+					Vector2 pixelPos=new Vector2(transform.position.x*Screen.width,transform.position.y*Screen.height);
+					Vector2 rectDimension=new Vector2((transform.localScale.x*Screen.width)/size,(transform.localScale.y*Screen.height)/size);				
+					rects[i,j]=NewRect(pixelPos.x-rectDimension.x*(i-size/2),pixelPos.y-rectDimension.y*(j-size/2));	
 				}
+					
 			}
 		}
 	
-		
+		public Rect NewRect(float centerX, float centerY, float width,float height){
+			return new Rect(centerX-width/2,centerY-height/2,width,height);
+		}
 	
 	}
 
